9ce6dcd99e92554bfb0b37e7e25bf49d2ec8281d
sultanmahmud07 <asadkhan01862@gmail.com>
2022-12-28 14:18:47 +0600

    media data loaded in media page

--- src/Pages/Media/Media.js
+++ src/Pages/Media/Media.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import '../../CommonStyle/Common.css'
+import PostCurd from './PostCurd/PostCurd';
 
 const Media = () => {
+  const  [medias, setMedias] = useState([]);
+
+
+  useEffect(() => {
+    fetch('http://localhost:5000/allMedia')
+    .then(res => res.json())
+    .then(data => setMedias(data))
+  },[])
+  console.log(medias)
   return (
-    <div>
-      <h1>This is Media section</h1>
+    <div className='common-w'>
+      <h1 className='text-3xl '>This is Media section</h1>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
+        {
+          medias.map(media => <PostCurd
+          key={media._id}
+          media={media}
+          ></PostCurd>)
+        }
+      </div>
     </div>
   );
 };
